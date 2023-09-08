2a5d969d56727e0d067b8921a61828215f10c079
James-Boarati <runaway_james@hotmail.com>
2023-09-07 21:54:12 -0300

    Criando validações

--- js/main.js
+++ js/main.js
@@ -3,7 +3,12 @@ const form = document.getElementById('novoItem')
 form.addEventListener('submit', (evento) => { // Submit está pedindo todos os dados, esses dados estou chamando de evento.
     evento.preventDefault() // previnindo o comportamento padrão desse evento.
 
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, quantidade)
 })
+
+function criaElemento (nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    document.createElement('li')
+}
