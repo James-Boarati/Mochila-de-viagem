e28426d06288d9de27031d2a5024f4849aeb7c97
James-Boarati <runaway_james@hotmail.com>
2023-09-08 21:47:08 -0300

    Criando validações visuais

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-const form = document.getElementById('novoItem')
+const form = document.getElementById("novoItem")
 
-form.addEventListener('submit', (evento) => { // Submit está pedindo todos os dados, esses dados estou chamando de evento.
+form.addEventListener("submit", (evento) => { // Submit está pedindo todos os dados, esses dados estou chamando de evento.
     evento.preventDefault() // previnindo o comportamento padrão desse evento.
 
-    criaElemento(evento.target.elements['nome'].value, quantidade)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
 })
 
-function criaElemento (nome, quantidade) {
-    console.log(nome)
-    console.log(quantidade)
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
 
-    document.createElement('li')
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+    
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
 }
